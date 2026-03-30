8a89926d76c4eb952a62dc4aec71399e6d8aa2d1
Ana Icu <25004137@galileo.edu>
2026-03-30 17:52:44 -0600

    Actualización

--- src/features/habit/habitSlice.ts
+++ src/features/habit/habitSlice.ts
@@ -72,7 +72,8 @@ const habitSlice = createSlice({
             state.error[action.meta.arg.habitId] = action.payload as string;
         }).addCase(fetchAddHabitThunk.fulfilled, (state, action) => {  
             console.log("action.payload:", action.payload);
-            state.habits.push(action.payload.habit);
+            //state.habits.push(action.payload);
+            state.habits.push(action.payload.habit ?? action.payload);
         });;
     }
 });
